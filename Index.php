85dff9000a3cc799f0562f51444f99792047fe23
aliciasacii <alicia@sopress.net>
2022-08-08 11:19:40 +0200

    corrections

--- Index.php
+++ Index.php
@@ -18,7 +18,7 @@
             <div id="description-header">
                 <span>Rapport de stage</span><br />
                 <span>Alicia Saci</span><br />
-                <span>SOPRESS</span><br /><br />
+                <span><a href="https://sopress.net/">SOPRESS</a></span><br /><br />
                 <span>11 Avril 2022 - 31 Août 2022</span><br />
             </div>
             <span>Tuteur entreprise : FRANCOIS Gilles</span><br />
@@ -30,23 +30,24 @@
                 <h1 class="big-title">1. Introduction</h1>
                 <h2>a. Objectif du présent rapport</h2>
                 <p>
-                    Dans le cadre de ma licence profesionnelle
+                    Dans le cadre de ma licence professionnelle
                     <b>CIASIE (Conception-Intégration d’Applications et Services Web
-                        pour l’Entreprise)</b>, J’ai eu l’opportunité d’effectuer mon stage de fin d’étude au sein du groupe
+                        pour l’entreprise)</b>, J’ai eu l’opportunité d’effectuer mon stage de fin d’études au sein du groupe
                     <b>SOPRESS</b>, ou j’ai rejoint l’équipe Dev/WEB et IT en tant que développeuse web pour une durée de 5 mois.<br /><br /> Durant cette période, j’ai pu découvrir les différentes manières de travailler au sein d’une équipe de développeurs
-                    et dans un contexte d'entreprise. j'ai pu côtoyer différents profils et apprendre de chacun.<br /><br /> En plus des très bonnes conditions de travail et de l'acceuil bienveillant de tous le personnel de la boite, les missions qui m'ont
-                    été confiées m'ont permit de monter progressivement en responsabilités, et de développer les soft-skills nécessaires pour mettre en valeur les travaux réalisés.<br /><br /> Ainsi, ce site me permet d'effectuer un compte rendu de mon
+                    et dans un contexte d'entreprise. j'ai pu côtoyer différents profils et apprendre de chacun.<br /><br /> En plus des très bonnes conditions de travail et de l'accueil bienveillant de tout le personnel de l'entreprise, les missions qui m'ont
+                    été confiées m'ont permis de monter progressivement en responsabilités, et de développer les soft-kills nécessaires pour mettre en valeur les travaux réalisés.<br /><br /> Ainsi, ce site me permet d'effectuer un compte rendu de mon
                     travail et de mon expérience vécue tout au long de mon stage.
                 </p>
                 <h2>Structure du rapport</h2>
                 <p>Ce rapport sera divisé en cinq grandes sections :<br /><br />
-                    1. La première partie est réservée à la présentation globale de l’entreprise qui m’a accueillie, son histoire ainsi que son domaine d’activité. Puis, de l’équipe que j’ai pu rejoindre.<br /><br />
-                    2. La deuxième partie détaillera les missions sur lesquelles je suis intervenu.<br /><br />
-                    4. La quatrième partie sur mon apport de mon stage et les différences avec la formation<br /><br />
-                    5. Une cinquième partie « Annexes » qui contiendra des illustrations et autres documents pertinents.</p>
+                    1. Une présentation globale de l’entreprise qui m’a accueillie, son histoire ainsi que son domaine d’activité. Puis, de l’équipe que j’ai pu rejoindre.<br /><br />
+                    2. Le détail des missions sur lesquelles je suis intervenu.<br /><br />
+                    3. L'apport du stage et les différences avec la formation<br /><br />
+                    4. Un « Annexes » qui contiendra des illustrations et autres documents pertinents.<br/><br/>
+                    5. Une conclusion.</p>
             </section>
             <div>
-                <a href="entreprise.php" class="pagination-section"><span>Page suivante</span><img id="next" src="images/right.png" rel="next"></a>
+                <a class="pagination-section" href="entreprise.php"><span>Page suivante</span><img id="next" src="images/right.png" rel="next"></a>
             </div>
         </div>
     </main>
--- entreprise.php
+++ entreprise.php
@@ -18,16 +18,16 @@
                 <h1 class="big-title">2. Contexte entreprise</h1>
                 <h2>a. SOPRESS</h2>
                 <p>
-                    Fondée il y a plus de 19 ans, SO Press est un groupe de press indépendant à l’origine de plusieurs titre de press. On y trouve SOFOOT -lancé en 2003- , un mensuel traitant du football sur un ton décalé. Et qui fut le tout premier magazine edité par le
+                    Fondée il y a plus de 19 ans, SO Press est un groupe de presse indépendant à l’origine de plusieurs titres de presse. On y trouve SOFOOT -lancé en 2003- un mensuel traitant du football sur un ton décalé. Et qui fut le tout premier magazine édité par le
                     groupe.
-                    <br /><br /> Plus tard, d’autre magazines traitants de sujet divers ont vu le jours. Le groupe propose désormais une déclinaison de SOFOOT pour les ados (So Foot Club), le quinzomadaire de société en liberté (Society), un média sur
+                    <br /><br /> Plus tard, d’autres magazines traitant de sujet divers ont vu le jour. Le groupe propose désormais une déclinaison de SOFOOT pour les ados (So Foot Club), le quinzomadaire de société en liberté (Society), un média sur
                     le cinéma (So Film), le vélo (Pédale), le rugby (Tampon), le basket (Trashtalk), les enfants pour les parents (Doolittle), l'élégance masculine (L'Etiquette), la musique d'aujourd'hui et de demain (Tsugi).
-                    <br /><br /> En plus de la presse écrite, SOPRESS a élargi son champ d’activité pour lancer une une régie publicitaire, H3 Media ainsi que des activités diversifiées dans la production audiovisuelle (AllSo, Sovage), l'édition de
+                    <br /><br /> En plus de la presse écrite, SOPRESS a élargi son champ d’activité pour lancer une régie publicitaire, H3 Media ainsi que des activités diversifiées dans la production audiovisuelle (AllSo, Sovage), l'édition de
                     livres (So Lonely), la musique (le label Vietnam), les podcasts (Allsound), l'événementiel et le brand content (print, web, vidéo, events)...
                 </p>
                 <h2>b. Focus sur le service DEV/WEB et IT :</h2>
                 <p>
-                    Au sein de <b>SOPRESS</b>, j’ai rejoins l’équipe DEV et IT, également connue sous le nom de
+                    Au sein de <b>SOPRESS</b>, j’ai rejoint l’équipe DEV et IT, également connue sous le nom de
                     <b>"l‘équipe des 0 et des 1"</b>. Elle forme le pôle informatique du groupe de presse.<br /><br /> L’équipe est actuellement composée de 3 personnes :
                 <ul>
                     <li>Gilles FRANCOIS : CTO.</li>
@@ -38,17 +38,17 @@
                 <ul>
                     <li>
                         Conception, réalisation et maintenance des multiples sites internet du groupe tel que sofoot.com, society-magazine.fr, sovage.tv, allso.tv, vietname-label.com ou encore solonely.fr.</li>
-                    <li>Mise en place de plusieurs outils interne à l’entreprise tel que SOGEST, qui est le système de gestion des employés de la boite ou encore "The Office" pour la gestion des bureaux.</li>
+                    <li>Mise en place de plusieurs outils interne à l’entreprise tel que SOGEST, qui est le système de gestion des employés de l'entreprise ou encore "The Office" pour la gestion des bureaux.</li>
 
                     <li>Gestion des incidents et assistance technique.</li>
 
                     <li>Accompagnement du personnel dans la prise en main des outils de l’entreprise tout en modernisant les méthodes de travail.</li>
 
                     <li>Constante proposition et exploration des nouvelles solutions et outils au sein de l’entreprise.</li>
-                    <li>Réalisation de projet pour des clients externes.</li><br />
+                    <li>Réalisation de projets pour des clients externes.</li><br />
                 </ul>
                 </p>
-                <p>Notre service est quotidiennement amené à collaborer avec d’autre services au sein de la boite, on retrouve notamment le service “DA maquette” ou l’on travaille avec des graphiste pour la création visuelle, le service juridique pour
+                <p>Notre service est quotidiennement amené à collaborer avec d’autres services au sein de l'entreprise, on retrouve notamment le service “DA maquette” pour la création visuelle, le service juridique pour
                     tout ce qui relève du droit internet ou encore le service abonnement.
                 </p>
                 </p>
