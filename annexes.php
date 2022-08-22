078ab2bfe6110410307f47662e978596e112f7a3
aliciasacii <alicia@sopress.net>
2022-08-22 14:53:32 +0200

    modifs

--- annexes.php
+++ annexes.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="js/script.js"></script>
     <link href="style/style.css" rel="stylesheet" />
-    <title>A</title>
+    <title>Annexes</title>
 </head>
 
 <body>
