9934bf23e3aed923f3a283a59d0a4f66f641ca08
alicia <aliciasaci@gmail.com>
2022-08-24 15:19:26 +0200

    mise à jour

--- apport_stage.php
+++ apport_stage.php
@@ -16,13 +16,13 @@
         <div id="main-content">
             <section id="formation">
                 <h1 class="big-title">4. Les apports de mon stage et les différences avec la formation</h1>
-                <p>
-                    J’ai été accompagnée durant mon stage par une équipe qui a fait preuve de beaucoup de patience et de bienveillance et qui s’est assuré, dès le début, de ma bonne intégration au sein de l’entreprise. Ils m’ont ainsi appris les bonnes pratiques et m’ont transmis le savoir et les conseils nécessaires pour mener à bien mes différentes missions.<br /><br />
+                <p class="alinea">
+                    J’ai été accompagnée durant mon stage par une équipe qui a fait preuve de beaucoup de patience et de bienveillance et qui s’est assuré, dès le début, de ma bonne intégration au sein de l’entreprise. Ils m’ont ainsi appris les bonnes pratiques et m’ont transmis le savoir et les conseils nécessaires pour mener à bien mes différentes missions.</p><br />
 
-                    Ce stage m’a permis de concrétiser mes acquis pédagogiques sur le terrain en touchant à différents les outils mis à disposition du Pôle informatique, mais aussi de découvrir de nouveaux aspects du développement web et ainsi d’avoir une vision globale et détaillée du métier.<br /><br />
+                <p class="alinea">Ce stage m’a permis de concrétiser mes acquis pédagogiques sur le terrain en touchant à différents les outils mis à disposition du Pôle informatique, mais aussi de découvrir de nouveaux aspects du développement web et ainsi d’avoir une vision globale et détaillée du métier.</p><br />
 
-                    Les différences entre la formation effectuée a l’université et le stage sont nombreuses. Par exemple, le fait de mener un projet de bout en bout, depuis la formulation des besoins par le client (interne ou externe) jusqu'à la mise en production du produit fini. Effectivement, une certaine rigueur est de mise pour bien comprendre les besoins et concevoir un produit répondant réellement à la demande. Il y a aussi beaucoup plus d’autonomie vis-à-vis du choix des technologies et de la conception de la solution contrairement aux projets universitaires où c’est souvent plus cadré et strict. Et enfin travailler auprès de développeurs plus expérimentés m’a permis de beaucoup apprendre et d’évoluer en tant que développeuse web.
-                </p>
+                <p class="alinea">Les différences entre la formation effectuée a l’université et le stage sont nombreuses. Par exemple, le fait de mener un projet de bout en bout, depuis la formulation des besoins par le client (interne ou externe) jusqu'à la mise en production du produit fini. Effectivement, une certaine rigueur est de mise pour bien comprendre les besoins et concevoir un produit répondant réellement à la demande. Il y a aussi beaucoup plus d’autonomie vis-à-vis du choix des technologies et de la conception de la solution contrairement aux projets universitaires où c’est souvent plus cadré et strict. Et enfin travailler auprès de développeurs plus expérimentés m’a permis de beaucoup apprendre et d’évoluer en tant que développeuse web.</p>
+
             </section>
             <div>
                 <a href="annexes.php" class="pagination-section"><span>Page suivante</span><img id="next" src="images/right.png" rel="next"></a>
