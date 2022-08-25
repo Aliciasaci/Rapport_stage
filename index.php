3d3c203f864d1e9990375a60d927d6aec4f8294e
alicia <aliciasaci@gmail.com>
2022-08-25 21:09:25 +0200

    mise à jour

--- index.php
+++ index.php
@@ -14,8 +14,7 @@
 <body>
     <?php include_once("header.php") ?>
     <main>
-        <section class="main-header">
-
+        <!-- <section class="main-header">
             <div><img src="images/logo_iut.jpg" style="border:none; width:35%; margin:30px auto;" /></div>
             <div id="description-header">
                 <span>Rapport de stage de Licence Professionnelle CIASIE</span><br />
@@ -27,38 +26,45 @@
             <span>Tuteur IUT : Yann BONIFACE</span><br /><br />
             <span>Année universitaire : 2021-2022</span>
         </section>
+        <br /><br /> -->
         <div id="main-content">
             <section>
-                <br /><br />
-                <h1 class="big-title">Remerciement</h1>
-                <p class="alinea">
-                    Je tiens en premier lieu à remercier mon tuteur de stage Gilles FRACOIS, ainsi qu'Aina RANDRIANARIJAONA pour m'avoir acceuili au sein de l'équipe et m'avoir fait confiance pour la réalisation des projets.</p><br />
-                    à continuer..
-                <p class="alinea"></p><br/><br/><br/></br><br/>
+                <section class="middle-section">
+                    <div class="third-section">
+                        <h1 class="big-title">Remerciement</h1>
+                        <p class="alinea">
+                            Je tiens en premier lieu à remercier mon tuteur de stage Gilles FRACOIS, ainsi qu'Aina RANDRIANARIJAONA pour m'avoir acceuili au sein de l'équipe et m'avoir fait confiance pour la réalisation des projets.</p><br />
+                        à continuer..
+                        <p class="alinea"></p><br /><br /><br /></br><br />
+                    </div>
+                    <hr />
+                    <div class="third-section">
+                        <h1 class="big-title">1. Introduction</h1>
+                        <p class="alinea">
+                            Dans le cadre de ma licence professionnelle CIASIE (Conception-Intégration d’Applications et Services Web pour l’entreprise), J’ai eu l’opportunité d’effectuer mon stage de fin d’études au sein du groupe SOPRESS, où j’ai rejoint l’équipe "Dev/WEB et IT" en tant que développeuse web pour une durée de 5 mois.</p>
 
-                <hr />
-                <h1 class="big-title">1. Introduction</h1>
-                <p class="alinea">
-                    Dans le cadre de ma licence professionnelle CIASIE (Conception-Intégration d’Applications et Services Web pour l’entreprise), J’ai eu l’opportunité d’effectuer mon stage de fin d’études au sein du groupe SOPRESS, où j’ai rejoint l’équipe "Dev/WEB et IT" en tant que développeuse web pour une durée de 5 mois.</p>
-
-                <br />
-                <p class="alinea">
-                    À travers ce rapport, je vais pouvoir détailler mon expérience vécue au sein du groupe SOPRESS, les missions auxquelles j’ai participé en tant que développeuse web, les outils et méthodes que j’ai pu utilisé ainsi que les différentes manières de travailler au sein d’une équipe de développeurs dans un contexte d'entreprise.
-                </p><br />
-                <h2>b. Structure du rapport</h2>
-                <p>Ce rapport sera divisé en cinq grandes sections :<br />
-                <ul>
-                    <li>La première partie est réservée à la présentation globale de l’entreprise qui m’a accueillie, son histoire ainsi que son domaine d’activité. Puis, de l’équipe que j’ai pu rejoindre.</li>
-                    <li>La deuxième partie abordera notre méthodologie de travail au sein de l'équipe, l'étude de l'existant ainsi que le détail des missions sur lesquelles je suis intervenu.</li>
-                    <li>La troisième concernera les apports de mon stage et les différences avec la formation.</li>
-                    <li>La quatrième partie conclura ce rapport.</li>
-                    <li>Et enfin une cinquième partie « Annexes » qui contiendra des illustrations et autres documents pertinents.</li>
-                </ul>
-                </p>
-            </section>
-            <div>
-                <a class="pagination-section" href="entreprise.php"><span>Page suivante</span><img id="next" src="images/right.png" rel="next"></a>
-            </div>
+                        <br />
+                        <p class="alinea">
+                            À travers ce rapport, je vais pouvoir détailler mon expérience vécue au sein du groupe SOPRESS, les missions auxquelles j’ai participé en tant que développeuse web, les outils et méthodes que j’ai pu utilisé ainsi que les différentes manières de travailler au sein d’une équipe de développeurs dans un contexte d'entreprise.
+                        </p><br />
+                    </div>
+                    <div class="third-section">
+                        <h2>b. Structure du rapport</h2>
+                        <p>Ce rapport sera divisé en cinq grandes sections :<br />
+                        <ul>
+                            <li>La première partie est réservée à la présentation globale de l’entreprise qui m’a accueillie, son histoire ainsi que son domaine d’activité. Puis, de l’équipe que j’ai pu rejoindre.</li>
+                            <li>La deuxième partie abordera notre méthodologie de travail au sein de l'équipe, l'étude de l'existant ainsi que le détail des missions sur lesquelles je suis intervenu.</li>
+                            <li>La troisième concernera les apports de mon stage et les différences avec la formation.</li>
+                            <li>La quatrième partie conclura ce rapport.</li>
+                            <li>Et enfin une cinquième partie « Annexes » qui contiendra des illustrations et autres documents pertinents.</li>
+                        </ul>
+                        </p>
+                    </div>
+                    <!-- <div>
+                        <a class="pagination-section" href="entreprise.php"><span>Page suivante</span><img id="next" src="images/right.png" rel="next"></a>
+                    </div> -->
+                </section>
+                <section>
         </div>
     </main>
     <?php include_once("footer.php") ?>
